37bd72ffdf1c434539380cae4a91eb8b987f4c11
billyvollman <b.vollman@gmail.com>
2020-02-02 14:13:32 +1100

    updated Burger component to check for empty array and applied messaging to site

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,7 +5,7 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 const burger = (props) => {
   // console.log(props)
   // debugger
-  const transformedIngredients = Object.keys(props.ingredients)
+  let transformedIngredients = Object.keys(props.ingredients)
     .map(igKey => {
       return [...Array(props.ingredients[igKey])].map((_, i) => {
         // console.log(igKey)
@@ -13,6 +13,14 @@ const burger = (props) => {
         return <BurgerIngredient key={igKey + i} type={igKey} />
       })
     })
+    .reduce((arr, el) => {
+      return arr.concat(el)
+    }, [])
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>
+  }
+  
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type='bread-top' />
