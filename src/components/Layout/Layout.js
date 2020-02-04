db5c8265c3df3c7401a8cb84c3b072acba4b3fc2
billyvollman <b.vollman@gmail.com>
2020-02-04 20:16:41 +1100

    changed side drawer to close if background clicked

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,32 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Aux from '../../hoc/Aux'
 import classes from './Layout.module.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
-const layout = (props) => (
-    <Aux>
+class Layout extends Component {
+
+  state = {
+    showSideDrawer: true
+  }
+
+  sideDrawerClosedHandler = () => {
+    this.setState({showSideDrawer: false})
+  }
+
+  render () {
+    return (
+      <Aux>
       < Toolbar />
-      < SideDrawer />
+      < SideDrawer 
+        open={this.state.showSideDrawer} 
+        closed={this.sideDrawerClosedHandler} />
       <main className={classes.Content}>
-        { props.children }
+        { this.props.children }
       </main>
     </Aux>
-)
+    )
+  }
+}
 
-export default layout
+export default Layout
